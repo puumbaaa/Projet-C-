e012cebbf8c5da3ab7bc43b8c45660f854ad924d
wdamour <wdamour@gaming.tech>
2024-03-19 15:30:43 +0100

    Add  object and CaseState

---
+++ Porjet C#/CaseState.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Porjet_C_
+{
+    internal class CaseState : Component
+    {
+        bool _isWalkable;
+        bool _isBorder;
+        bool _isGrass;
+
+        public bool IsWalkable { get => _isWalkable; set => _isWalkable = value; }
+        public bool IsBorder { get => _isBorder; set => _isBorder = value; }
+        public bool IsGrass { get => _isGrass; set => _isGrass = value; }
+
+        public CaseState(string name, bool walkable, bool border, bool grass) : base(name) 
+        {
+            IsWalkable = walkable;
+            IsBorder = border;
+            IsGrass = grass;
+        }
+
+        public bool StartFight()
+        {
+            if (IsGrass)
+            {
+                Random rnd = new Random();
+                if (rnd.Next(100) <= 20)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Porjet C#/Object.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Porjet_C_
+{
+    internal class Object : Component
+    {
+        string _statName;
+        float _statValue;
+        bool _isKey;
+
+        public string StatName { get => _statName; private set => _statName = value; }
+        public float StatValue { get => _statValue; private set => _statValue = value; }
+        public bool IsKey { get => _isKey; set => _isKey = value; }
+
+        public Object(string name, string statName, float statValue) : base(name) 
+        {
+            StatName = statName;
+            StatValue = statValue;
+        } 
+
+        public Object(string name) : base(name) 
+        {
+            IsKey = true;
+        }
+    }
+}
