015b38c38863aa677647c99e2d376f2367ebe321
glacox <lorisfiard1@gmail.com>
2024-03-20 11:10:30 +0100

    grid + enemy

--- Porjet C#/FileReader.cs
+++ Porjet C#/FileReader.cs
@@ -18,20 +18,43 @@ namespace Mapp
             private set => m_sText = value;
         }
 
+        public int GetLineCount(string file)
+        {
+            if (File.Exists(file))
+            {
+                return File.ReadLines(file).Count();
+            }
+            else
+            {
+                return 0;
+            }
+        }
 
-        public void setFile(string file)
+
+        public void SetFile(string file)
         {
             if (File.Exists(file))
             {
                 // Read entire text file content in one string
-                 m_sText = File.ReadAllText(file);
+                m_sText = File.ReadAllText(file);
             }
         }
 
-        public void printFile(string file)
+
+
+        public void PrintFile(string file)
         {
-            setFile(file);
+            SetFile(file);
             Console.WriteLine(sText);
         }
-    }
+
+        public void PrintFileLine(string file, int line)
+        {
+            string m_sText = File.ReadLines(file).Skip(line).Take(1).First();
+            Console.WriteLine(m_sText);
+
+        }
+
+
+    } // End of class
 }
