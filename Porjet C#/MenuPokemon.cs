20d07ca1c1fdd3ba5a886d2d82657b25404b9011
wdamour <wdamour@gaming.tech>
2024-03-21 15:57:49 +0100

    Menu pokiemon a changer (trop de if)

--- Porjet C#/MenuPokemon.cs
+++ Porjet C#/MenuPokemon.cs
@@ -19,22 +19,29 @@ namespace Porjet_C_
 
         public void DisplayPokemonMenue(FileReader map, int itemValue)
         {
+            //Still don't know what i'm doing 
+            Pokemon pokemon = (Pokemon)Pokemons.ComponentsList[itemValue];
+
             string menuPokemon = DisplayMenu(map, Pokemons, itemValue);
             FileReader newFileReader = new FileReader();
             newFileReader.setFile("..\\..\\..\\..\\ASCII\\Menu\\menuPokemontotal.txt");
             string finalMap = "";
+            int nbDash = 0;
             for (int i = 0; i < menuPokemon.Length; i++)
             {
-                if (newFileReader.sText[i]=='.')
+                if (newFileReader.sText[i] == '.')
                 {
                     finalMap += menuPokemon[i];
-                }else if (newFileReader.sText[i] != '_')
+                } else if (newFileReader.sText[i] != '_')
                 {
                     finalMap += newFileReader.sText[i];
+                }else
+                {
+                    //don't know what i'mp doing
+                    nbDash++;                    
                 }
+
             }
         }
-
-
     }
 }
