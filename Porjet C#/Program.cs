e9a69be6e69ce54744e5b21b319b2069cb952881
glacox <lorisfiard1@gmail.com>
2024-03-21 15:20:46 +0100

    other case system with other sprites condition

--- Porjet C#/Program.cs
+++ Porjet C#/Program.cs
@@ -24,7 +24,6 @@ namespace test
             |                                                                                     |
             *///----------------------------------------------------------------------------------|
 
-            string sMap = "..\\..\\..\\..\\ASCII\\Map\\map.txt";
             string sCombat = "..\\..\\..\\..\\ASCII\\Scenes\\combat.txt";
             string sMonster1 = "..\\..\\..\\..\\ASCII\\Sprites\\monster1.txt";
 
@@ -36,7 +35,6 @@ namespace test
             |                                                                                     |
             *///----------------------------------------------------------------------------------|
 
-            FileReader mapFile = new FileReader();
             FileReader combatGridFile = new FileReader();
             FileReader monster1File = new FileReader();
 
@@ -48,7 +46,6 @@ namespace test
             |                                                                                     |
             *///----------------------------------------------------------------------------------|
 
-            mapFile.SetFile(sMap);
             combatGridFile.SetFile(sCombat);
             monster1File.SetFile(sMonster1);
 
@@ -60,7 +57,6 @@ namespace test
             |                                                                                     |
             *///----------------------------------------------------------------------------------|
 
-            Map map = new Map();
             Grid grid = new Grid();
 
 
@@ -85,8 +81,6 @@ namespace test
             *///----------------------------------------------------------------------------------|
             combatGridFile.printFile();
 
-            Console.WriteLine(mapFile.sText.Length);
-            map.mapSet(mapFile.sText);
 
             Console.SetCursorPosition(0, 0);
             Console.SetBufferSize(120, 31);
@@ -141,7 +135,6 @@ namespace test
             int x = 60;
             int y = 15;
             Console.SetCursorPosition(x, y);
-            Console.WriteLine("P");
             Console.CursorVisible = false;
 
             while (true) // Game Loop
@@ -237,7 +230,7 @@ namespace test
                 combatGridFile.printFile();
 
                 // Draw Enemy
-                for (int i = 0; i < mapFile.GetLineCount(sMonster1); i++)
+                for (int i = 0; i < monster1File.GetLineCount(sMonster1); i++)
                 {
                     Console.SetCursorPosition(grid.combatGrid[grid.m_Case, 0], grid.combatGrid[grid.m_Case, 1] + i);
                     monster1File.PrintFileLine(sMonster1, i);
